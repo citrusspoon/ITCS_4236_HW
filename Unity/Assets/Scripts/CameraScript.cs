21d5748585e86bde5a41c604f185bafe020e49cc
Unknown <citrusspoon@gmail.com>
2017-09-06 14:25:41 -0400

    Started working on kinematic

--- Unity/Assets/Scripts/CameraScript.cs
+++ Unity/Assets/Scripts/CameraScript.cs
@@ -12,9 +12,9 @@ public class CameraScript : MonoBehaviour {
 		
 	}
 		
-	void OnMouseDown(){
-		print (Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
-			Input.mousePosition.y, Camera.main.nearClipPlane)));
+	public Vector3 getMouseLocation(){
+		return Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+			Input.mousePosition.y, Camera.main.nearClipPlane));
 	}
 	
 	// Update is called once per frame
--- Unity/Assets/Scripts/KinematicArrive.cs
+++ Unity/Assets/Scripts/KinematicArrive.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class KinematicArrive : MonoBehaviour {
 
+	public GameObject character;
+	private Vector3 target;
+	public float maxSpeed;
+	public float satRadius;
+	public float timeToTarget;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
+
+	void setTarget(Vector3 m){
+		target = m;
+	}
+
 	
 	// Update is called once per frame
 	void Update () {
