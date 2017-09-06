ce1e72d86f612ee6e038217200ba525aefdd1dc7
Unknown <citrusspoon@gmail.com>
2017-09-06 14:19:11 -0400

    Got camera script working maybe

---
+++ Unity/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+		
+	void OnMouseDown(){
+		print (Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+			Input.mousePosition.y, Camera.main.nearClipPlane)));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+		// Get the mouse position from Event.
+		// Note that the y position from Event is inverted.
+
+	}
+}
