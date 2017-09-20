1371cd7b37d79366f530583463e89a05be3ddede
Unknown <citrusspoon@gmail.com>
2017-09-20 13:51:18 -0400

    Made obstacle spawning function

--- Unity/Assets/Scripts/PlaneScript.cs
+++ Unity/Assets/Scripts/PlaneScript.cs
@@ -6,15 +6,23 @@ public class PlaneScript : MonoBehaviour
 {
 
 	public GameObject character;
+	private GameObject obstacleClone;
+	public GameObject obstaclePrefab;
+
 
 
 	// Use this for initialization
 	void Start ()
 	{
 
-	}
 
-	void OnMouseDown ()
+
+
+	}
+	/// <summary>
+	/// Sets the target point for Kinematic Arrive
+	/// </summary>
+	void leftClick ()
 	{
 
 		//sends mouse click position to the KinematicArrive script
@@ -22,12 +30,28 @@ public class PlaneScript : MonoBehaviour
 			Input.mousePosition.y, Camera.main.nearClipPlane)));
 
 	}
+	/// <summary>
+	/// Creates an obstacle at clicked point.
+	/// </summary>
+	void rightClick ()
+	{
+		Vector3 clickPoint = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+		obstacleClone = Instantiate (obstaclePrefab, new Vector3(clickPoint.x, clickPoint.y, 0.0f) , Quaternion.identity) as GameObject;
+
+	
+
+
+	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
 		
+		if (Input.GetMouseButtonDown (0))
+			leftClick ();
 
+		if (Input.GetMouseButtonDown (1))
+			rightClick ();
 
 	}
 }
