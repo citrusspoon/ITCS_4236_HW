302105b4bfddd84fc0504383cfb1e2bcd89fa7e4
Scott Schreiber <citrusspoon@gmail.com>
2017-10-12 16:49:34 -0400

    Implemented decompose method. doesn't quite work

--- Unity/Assets/Scripts/DecomposerScript.cs
+++ Unity/Assets/Scripts/DecomposerScript.cs
@@ -2,28 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class DecomposerScript : MonoBehaviour {
 
 	public int planeLength, planeHeight;
+	public Node[,] grid;
+
 	// Use this for initialization
 	void Start () {
-		
+		grid = new Node[50,50];
+	}
+
+	void drawLines(){
+		for (int i = 0; i < planeLength; i++) {
+			for (int j = 0; j < planeHeight; j++) {
+				if(grid[i,j].isPathable())
+					Debug.DrawLine (new Vector3 (i + 0.5f, j + 0.5f, -2), new Vector3 (i + 0.5f, j + 0.5f, 1), Color.green);
+				else
+					Debug.DrawLine (new Vector3 (i + 0.5f, j + 0.5f, -2), new Vector3 (i + 0.5f, j + 0.5f, 1), Color.blue);
+			}
+		}
 	}
 
 	void decompose(){
 
-		for(int i = 0; i < planeLength; i++)
-			for(int j = 0; j < planeHeight; j++)
-				Debug.DrawLine (new Vector3(i + 0.5f,j+0.5f,-2), new Vector3(i + 0.5f,j+0.5f,1), Color.green);
+		RaycastHit hit;
 
+		for (int i = 0; i < planeLength; i++) {
+			for (int j = 0; j < planeHeight; j++) {
+
+				if (Physics.Raycast (new Vector3 (i + 0.5f, j + 0.5f, -2), new Vector3 (i + 0.5f, j + 0.5f, 1), out hit)) {
+					if (hit.collider.gameObject.tag == "Obstacle") {
+						grid [i, j] = new Node (false);
+						print ("Unpathable found");
+					} else {
+						grid [i, j] = new Node (true);
+					}
+				}
+				else
+					grid [i, j] = new Node (true);
+
+			}
+		}
+				
+		print ("Decompose finished");
 
 		
 
 	}
+		
+
 	
 	// Update is called once per frame
 	void Update () {
 
-		decompose ();
+		if(Input.GetKey(KeyCode.LeftControl))
+			drawLines ();
+
+		if(Input.GetKeyDown(KeyCode.Space))
+			decompose ();
 	}
 }
---
+++ Unity/Assets/Scripts/Node.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Node {
+
+	private bool pathable;
+
+	public Node(bool b){
+		pathable = b;
+	}
+
+	public bool isPathable(){
+		return pathable;
+	}
+
+}
